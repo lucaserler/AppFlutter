760ef6fe5e2699f3a0209f518bc09af482184fb9
Lucas Erler <115714717+lucaserler@users.noreply.github.com>
2025-02-27 14:42:16 -0300

    v0.7 GetX - Routes

---
+++ lib/src/pages_routes/app_pages.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:greengrocer/src/pages/auth/sign_in_screen.dart';
+import 'package:greengrocer/src/pages/auth/sign_up_screen.dart';
+import 'package:greengrocer/src/pages/base/base_screen.dart';
+import 'package:greengrocer/src/pages/splash/splash_screen.dart';
+
+abstract class AppPages {
+  static final pages = <GetPage>[
+    GetPage(
+      page: () => SplashScreen(),
+      name: PagesRoutes.splashRoute,
+    ),
+    GetPage(
+      page: () => SignInScreen(),
+      name: PagesRoutes.signInRoute,
+    ),
+    GetPage(
+      page: () => SignUpScreen(),
+      name: PagesRoutes.signUpRoute,
+    ),
+    GetPage(
+      page: () => BaseScreen(),
+      name: PagesRoutes.baseRoute,
+    )
+  ];
+}
+
+abstract class PagesRoutes {
+  static String splashRoute = '/splash';
+  static String signInRoute = '/signin';
+  static String signUpRoute = '/signup';
+  static String baseRoute = '/';
+}
